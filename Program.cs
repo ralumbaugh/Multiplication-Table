afc00528575c316c7f9ad1984a65f630bab3a73e
Ryan Alumbaugh <ralumbaugh@gmail.com>
2020-06-01 15:28:09 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] Table = new int[10,10];
+            for(int column=0; column<10; column++)
+            {
+                Table[column,0] = column+1;
+                for(int row=1; row<10; row++)
+                {
+                    Table[column,row] = (row+1)*(column+1);
+                }
+            }
+
+            for(int i=0; i<10; i++)
+            {
+                string line = "[";
+                for(int j=0; j<10; j++)
+                {
+                    line = $"{line} {Table[i,j]}, ";
+                }
+                line = line + "]";
+                Console.WriteLine(line);
+            }
+        }
+
+    }
+}
